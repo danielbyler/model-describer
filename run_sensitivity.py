ae88a6d770553a113ccc4e56028c1c53013a5efc
Jason Lewris <datawrestler@gmail.com>
2018-01-29 16:41:52 -0500

    added testing scenario for sensitivity branch

---
+++ run_sensitivity.py
@@ -0,0 +1,6 @@
+# sensitivity plot creation and testing
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+from whitebox import utils
+import numpy as np
+from whitebox.wbox_error import WhiteBoxError
