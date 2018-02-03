73c33737f38944cfb2a07dedb1cd35cc96194a9a
Jason Lewris <datawrestler@gmail.com>
2018-02-02 22:26:03 -0500

    setup config

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='WhiteBox_Production',
+    name='whitebox',
     version='0.0.1',
-    packages=['', 'WhiteBox_Production'],
+    packages=['whitebox'],
     url='https://github.com/Data4Gov/WhiteBox_Production',
     license='MIT',
     author='Jason Lewris, Daniel Byler, Venkat Gangavarapu, Shruti Panda',
