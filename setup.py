9c5200e4bc208d6a8741765aefddfb78d6618dd2
Jason Lewris <datawrestler@gmail.com>
2018-02-08 14:19:32 -0500

    update package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='whitebox',
-    version='0.0.7.4',
+    version='0.0.7.6',
     packages=['whitebox'],
     url='https://github.com/Data4Gov/WhiteBox_Production',
     license='MIT',
@@ -11,5 +11,5 @@ setup(
     description='Unlock model details and shine light on how your data is performing within regions of the model',
     download_url='https://github.com/data4gov/WhiteBox_Production/archive/v0.0.6.tar.gz',
     keywords=['whitebox', 'machine learning', 'data science'],
-    package_data={'whitebox': ['*.txt']},
+    package_data={'whitebox': ['html_error.txt', 'html_sensitivity.txt']},
 )
