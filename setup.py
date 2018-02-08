cca4bbe625b9cc5bc7342a11bd6c6442b423e3be
Jason Lewris <datawrestler@gmail.com>
2018-02-07 20:31:42 -0500

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='whitebox',
-    version='0.0.3',
+    version='0.0.4',
     packages=['whitebox'],
     url='https://github.com/Data4Gov/WhiteBox_Production',
     license='MIT',
     author='Jason Lewris, Daniel Byler, Venkat Gangavarapu, Shruti Panda',
     author_email='jlewris@deloitte.com',
     description='Unlock model details and shine light on how your data is performing within regions of the model',
-    download_url='https://github.com/data4gov/WhiteBox_Production/archive/v0.0.3.tar.gz',
+    download_url='https://github.com/data4gov/WhiteBox_Production/archive/v0.0.4.tar.gz',
     keywords=['whitebox', 'machine learning', 'data science']
 )
