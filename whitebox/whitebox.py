e1834babe7d2ceeff46f115d6bab42e37ea26a39
Jason Lewris <datawrestler@gmail.com>
2018-02-07 20:31:08 -0500

    further resolved imports

--- whitebox/whitebox.py
+++ whitebox/whitebox.py
@@ -8,8 +8,12 @@ import numpy as np
 from pandas import DataFrame, concat
 from pandas.api.types import is_categorical_dtype
 
-from whitebox.utils import to_json
-from whitebox_base import WhiteBoxBase
+try:
+    from whitebox.utils import to_json
+    from whitebox.whitebox_base import WhiteBoxBase
+except:
+    from utils import to_json
+    from whitebox_base import WhiteBoxBase
 
 __author__ = """Jason Lewris, Daniel Byler, Venkat Gangavarapu, 
                 Shruti Panda, Shanti Jha"""
