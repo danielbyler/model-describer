5b8cab7c71534c2c6475e53eb1ca6095c5087a60
danielbyler <dbyler@deloitte.com>
2018-02-21 15:25:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Jason Lewris, Daniel Byler, Venkat Gangavarapu, Shruti Panda',
     author_email='jlewris@deloitte.com',
     description="""How can I unlock what my model is thinking? WhiteBox helps answer this problem for sklearn machine learning models. 
-    Specifically, WhiteBox addresses two key issues: error and model sensitivity. For error, WhiteBox analyzes how well the model
+    Specifically, WhiteBox helps address two key issues: error and model sensitivity. For error, WhiteBox analyzes how well the model
     is performing within key regions of the data in a visually compelling way. For sensitivity, WhiteBox identifies what parts of 
     different variable distributions have the biggest impact on model predictions and plots them. All WhiteBox output is created with 
     simple code and produces HTML files that are small enough to be emailed.""",
