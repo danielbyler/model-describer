84d288a596e75598332d1f9c965428c8cc4e813c
datawrestler <datawrestler@gmail.com>
2018-03-06 20:23:33 -0500

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='model-describer',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages("."),
     url='https://github.com/DataScienceSquad/model-describer',
     license='MIT',
